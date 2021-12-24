ecbbd8a1fca0ac7209c812fed93975c4785fcc03
Andrew <andreybogdanov88@gmail.com>
2021-12-24 11:13:36 +0200

    simplifying themeConfig.js

--- theme/themeConfig.js
+++ theme/themeConfig.js
@@ -1,5 +1,5 @@
 // default configuration for links and images
-const defaultConfig = {
+export default {
   home: {
     bannerA: {
       link: '/',
@@ -22,34 +22,3 @@ const defaultConfig = {
     }
   }
 };
-// configuration used for links and images for demo purposes
-const demoConfig = {
-  home: {
-    bannerA: {
-      link: '/c/women/women-clothing-skirts',
-      image: {
-        mobile: '/homepage/bannerB.webp',
-        desktop: '/homepage/bannerF.webp'
-      }
-    },
-    bannerB: {
-      link: '/c/women/women-clothing-dresses',
-      image: '/homepage/bannerE.webp'
-    },
-    bannerC: {
-      link: '/c/women/women-clothing-shirts',
-      image: '/homepage/bannerC.webp'
-    },
-    bannerD: {
-      link: '/c/women/women-shoes-sandals',
-      image: '/homepage/bannerG.webp'
-    }
-  }
-};
-
-export default (() => {
-  if (process.env.IS_DEMO) {
-    return demoConfig;
-  }
-  return defaultConfig;
-})();
