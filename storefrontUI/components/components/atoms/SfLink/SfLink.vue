471818c60c471fb6e8a38f5d136ea85c6ca261dd
Andrew <andreybogdanov88@gmail.com>
2022-01-24 14:30:08 +0200

    fix: faas-1522 ejecting vsf-ui components

---
+++ storefrontUI/components/components/atoms/SfLink/SfLink.vue
@@ -0,0 +1,48 @@
+<template functional>
+  <component
+    :is="$options.linkComponentTag($options.isExternal, $nuxt, $router)"
+    v-bind="
+      $options.attributes($options.isExternal, $router, props.link, data.attrs)
+    "
+    :class="[data.class, data.staticClass, 'sf-link']"
+    v-on="listeners"
+  >
+    <!-- @slot -->
+    <slot />
+  </component>
+</template>
+<script>
+import { focus } from "../../../utilities/directives";
+export default {
+  name: "SfLink",
+  directives: { focus },
+  props: {
+    /**
+     * Page route
+     */
+    link: {
+      type: [String, Object],
+      default: "",
+    },
+  },
+  isExternal() {
+    return (
+      typeof this.link === "string" && this.link.search(/(^\/|^#)/g) === -1
+    );
+  },
+  attributes(isExternal, $router, link, attrs) {
+    const urlTag = isExternal || !$router ? { href: link } : { to: link };
+    return {
+      ...attrs,
+      ...urlTag,
+    };
+  },
+  linkComponentTag(isExternal, $nuxt, $router) {
+    const routerLink = $nuxt ? "nuxt-link" : "router-link";
+    return isExternal || !$router ? "a" : routerLink;
+  },
+};
+</script>
+<style lang="scss">
+@import "~@storefront-ui/shared/styles/components/atoms/SfLink.scss";
+</style>
---
+++ storefrontUI/components/index.js
@@ -0,0 +1,150 @@
+// Auto-generated file by create-index-files.js. Do not edit manually
+import SfArrow from "./components/atoms/SfArrow/SfArrow.vue";
+import SfBadge from "./components/atoms/SfBadge/SfBadge.vue";
+import SfBreadcrumbs from "./components/atoms/SfBreadcrumbs/SfBreadcrumbs.vue";
+import SfBullets from "./components/atoms/SfBullets/SfBullets.vue";
+import SfButton from "./components/atoms/SfButton/SfButton.vue";
+import SfChevron from "./components/atoms/SfChevron/SfChevron.vue";
+import SfCimage from "./components/atoms/SfCimage/SfCimage.vue";
+import SfCircleIcon from "./components/atoms/SfCircleIcon/SfCircleIcon.vue";
+import SfColor from "./components/atoms/SfColor/SfColor.vue";
+import SfDivider from "./components/atoms/SfDivider/SfDivider.vue";
+import SfHeading from "./components/atoms/SfHeading/SfHeading.vue";
+import SfIcon from "./components/atoms/SfIcon/SfIcon.vue";
+import SfImage from "./components/atoms/SfImage/SfImage.vue";
+import SfInput from "./components/atoms/SfInput/SfInput.vue";
+import SfLink from "./components/atoms/SfLink/SfLink.vue";
+import SfLoader from "./components/atoms/SfLoader/SfLoader.vue";
+import SfOverlay from "./components/atoms/SfOverlay/SfOverlay.vue";
+import SfPrice from "./components/atoms/SfPrice/SfPrice.vue";
+import SfProperty from "./components/atoms/SfProperty/SfProperty.vue";
+import SfQuantitySelector from "./components/atoms/SfQuantitySelector/SfQuantitySelector.vue";
+import SfRating from "./components/atoms/SfRating/SfRating.vue";
+import SfTextarea from "./components/atoms/SfTexarea/SfTextarea.vue";
+import SfAddressPicker from "./components/molecules/SfAddressPicker/SfAddressPicker.vue";
+import SfAddToCart from "./components/molecules/SfAddToCart/SfAddToCart.vue";
+import SfAlert from "./components/molecules/SfAlert/SfAlert.vue";
+import SfBanner from "./components/molecules/SfBanner/SfBanner.vue";
+import SfBar from "./components/molecules/SfBar/SfBar.vue";
+import SfBottomModal from "./components/molecules/SfBottomModal/SfBottomModal.vue";
+import SfCallToAction from "./components/molecules/SfCallToAction/SfCallToAction.vue";
+import SfCard from "./components/molecules/SfCard/SfCard.vue";
+import SfCategoryCard from "./components/molecules/SfCategoryCard/SfCategoryCard.vue";
+import SfCharacteristic from "./components/molecules/SfCharacteristic/SfCharacteristic.vue";
+import SfCheckbox from "./components/molecules/SfCheckbox/SfCheckbox.vue";
+import SfColorPicker from "./components/molecules/SfColorPicker/SfColorPicker.vue";
+import SfComponentSelect from "./components/molecules/SfComponentSelect/SfComponentSelect.vue";
+import SfDropdown from "./components/molecules/SfDropdown/SfDropdown.vue";
+import SfFilter from "./components/molecules/SfFilter/SfFilter.vue";
+import SfGallery from "./components/molecules/SfGallery/SfGallery.vue";
+import SfMenuItem from "./components/molecules/SfMenuItem/SfMenuItem.vue";
+import SfModal from "./components/molecules/SfModal/SfModal.vue";
+import SfNotification from "./components/molecules/SfNotification/SfNotification.vue";
+import SfPagination from "./components/molecules/SfPagination/SfPagination.vue";
+import SfProductOption from "./components/molecules/SfProductOption/SfProductOption.vue";
+import SfRadio from "./components/molecules/SfRadio/SfRadio.vue";
+import SfRange from "./components/molecules/SfRange/SfRange.vue";
+import SfReview from "./components/molecules/SfReview/SfReview.vue";
+import SfScrollable from "./components/molecules/SfScrollable/SfScrollable.vue";
+import SfSearchBar from "./components/molecules/SfSearchBar/SfSearchBar.vue";
+import SfSection from "./components/molecules/SfSection/SfSection.vue";
+import SfSelect from "./components/molecules/SfSelect/SfSelect.vue";
+import SfSlidingSection from "./components/molecules/SfSlidingSection/SfSlidingSection.vue";
+import SfSteps from "./components/molecules/SfSteps/SfSteps.vue";
+import SfSticky from "./components/molecules/SfSticky/SfSticky.vue";
+import SfTile from "./components/molecules/SfTile/SfTile.vue";
+import SfAccordion from "./components/organisms/SfAccordion/SfAccordion.vue";
+import SfBannerGrid from "./components/organisms/SfBannerGrid/SfBannerGrid.vue";
+import SfBottomNavigation from "./components/organisms/SfBottomNavigation/SfBottomNavigation.vue";
+import SfCarousel from "./components/organisms/SfCarousel/SfCarousel.vue";
+import SfCollectedProduct from "./components/organisms/SfCollectedProduct/SfCollectedProduct.vue";
+import SfContentPages from "./components/organisms/SfContentPages/SfContentPages.vue";
+import SfFooter from "./components/organisms/SfFooter/SfFooter.vue";
+import SfGroupedProduct from "./components/organisms/SfGroupedProduct/SfGroupedProduct.vue";
+import SfHeader from "./components/organisms/SfHeader/SfHeader.vue";
+import SfHero from "./components/organisms/SfHero/SfHero.vue";
+import SfList from "./components/organisms/SfList/SfList.vue";
+import SfMegaMenu from "./components/organisms/SfMegaMenu/SfMegaMenu.vue";
+import SfProductCard from "./components/organisms/SfProductCard/SfProductCard.vue";
+import SfProductCardHorizontal from "./components/organisms/SfProductCardHorizontal/SfProductCardHorizontal.vue";
+import SfSidebar from "./components/organisms/SfSidebar/SfSidebar.vue";
+import SfStoreLocator from "./components/organisms/SfStoreLocator/SfStoreLocator.vue";
+import SfTable from "./components/organisms/SfTable/SfTable.vue";
+import SfTabs from "./components/organisms/SfTabs/SfTabs.vue";
+import SfTopBar from "./components/organisms/SfTopBar/SfTopBar.vue";
+
+export {
+  SfArrow,
+  SfBadge,
+  SfBreadcrumbs,
+  SfBullets,
+  SfButton,
+  SfChevron,
+  SfCimage,
+  SfCircleIcon,
+  SfColor,
+  SfDivider,
+  SfHeading,
+  SfIcon,
+  SfImage,
+  SfInput,
+  SfLink,
+  SfLoader,
+  SfOverlay,
+  SfPrice,
+  SfProperty,
+  SfQuantitySelector,
+  SfRating,
+  SfTextarea,
+  SfAddressPicker,
+  SfAddToCart,
+  SfAlert,
+  SfBanner,
+  SfBar,
+  SfBottomModal,
+  SfCallToAction,
+  SfCard,
+  SfCategoryCard,
+  SfCharacteristic,
+  SfCheckbox,
+  SfColorPicker,
+  SfComponentSelect,
+  SfDropdown,
+  SfFilter,
+  SfGallery,
+  SfMenuItem,
+  SfModal,
+  SfNotification,
+  SfPagination,
+  SfProductOption,
+  SfRadio,
+  SfRange,
+  SfReview,
+  SfScrollable,
+  SfSearchBar,
+  SfSection,
+  SfSelect,
+  SfSlidingSection,
+  SfSteps,
+  SfSticky,
+  SfTile,
+  SfAccordion,
+  SfBannerGrid,
+  SfBottomNavigation,
+  SfCarousel,
+  SfCollectedProduct,
+  SfContentPages,
+  SfFooter,
+  SfGroupedProduct,
+  SfHeader,
+  SfHero,
+  SfList,
+  SfMegaMenu,
+  SfProductCard,
+  SfProductCardHorizontal,
+  SfSidebar,
+  SfStoreLocator,
+  SfTable,
+  SfTabs,
+  SfTopBar
+};
