f79715571615d985858774b353e1412e593aee03
dunqan <dunqan@w9.pl>
2022-01-25 14:17:28 +0100

    optimize middleware

--- functions/middleware.js
+++ functions/middleware.js
@@ -1,31 +1,6 @@
 const serverless = require('serverless-http');
-
 const { createServer } = require('@vue-storefront/middleware');
-
-const middlewareConfig = {
-  integrations: {
-    spryker: {
-      location: '@spryker-vsf/api/server',
-      configuration: {
-        axiosConfig: {
-          baseURL:
-            process.env.GLUE_BASE_URL ||
-            process.env.API_URL ||
-            'https://glue.us.sc-b2b.demo-spryker.com',
-        },
-        currency: process.env.CURRENCY_DEFAULT || 'USD',
-        locale: process.env.LOCALE_DEFAULT || 'en_US',
-      },
-
-      extensions: (extensions) => [
-        ...extensions,
-        require('@spryker-vsf/catalog-search-suggestions/extension'),
-        require('@spryker-vsf/oidc-client/extension'),
-        require('@spryker-vsf/oidc/extension'),
-      ],
-    },
-  },
-};
+const middlewareConfig = require('../middleware.config');
 
 function middlewareHandler() {
     const app = createServer(middlewareConfig);
