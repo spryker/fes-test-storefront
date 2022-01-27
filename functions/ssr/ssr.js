fb55145aded7127e896c2c0934dbe776df5e5c27
Stan <stanislaw.stanislawski@spryker.com>
2022-01-27 17:47:41 +0100

    feat: ssr via builder (#24)

--- functions/ssr/ssr.js
+++ functions/ssr/ssr.js
@@ -1,6 +1,7 @@
 const serverless = require('serverless-http');
 const webpack = require('webpack');
 const { Nuxt } = require('nuxt-start');
+const { builder } = require("@netlify/functions")
 
 const currencies = process.env.CURRENCIES
   ? process.env.CURRENCIES.split(',').map((currency) => ({
@@ -207,25 +208,27 @@ function createNuxtHandler(nuxtConfig) {
   });
 
   let server = null;
-  return async (event, ctx, callback) => {
+  return async (event, ctx) => {
+
     if (!server) {
       await nuxt?.ready();
       server = serverless(nuxt.server.app);
     }
 
-    const result = await server(event, ctx, callback);
-    const cacheValue = event.queryStringParameters.ebPreview
-      ? 'no-cache'
-      : 'public, max-age=31536000';
-
-    return {
-      ...result,
-      headers: {
-        ...result.headers,
-        'Cache-Control': cacheValue,
-      },
-    };
+    const result = await server(event, ctx);
+    // const cacheValue = event.queryStringParameters.ebPreview
+    //   ? 'no-cache'
+    //   : 'public, max-age=31536000';
+    //
+    // return {
+    //   ...result,
+    //   headers: {
+    //     ...result.headers,
+    //     'Cache-Control': cacheValue,
+    //   },
+    // };
+    return result;
   };
 }
 
-exports.handler = createNuxtHandler(config);
+exports.handler = builder(createNuxtHandler(config));
