5166985f3c9cfcf1772569cf5236ea1dd87365db
tobi-or-not-tobi <tobiasouwejan@gmail.com>
2022-01-31 11:50:19 +0100

    disable mapping by default

--- customizations/custom-mapping.js
+++ customizations/custom-mapping.js
@@ -1,5 +1,5 @@
 export const mapping = {
-  'oryx-banner': {
-    componentFactory: () => import('./CustomBanner.vue'),
-  },
+  // 'oryx-banner': {
+  //   componentFactory: () => import('./CustomBanner.vue'),
+  // },
 };
