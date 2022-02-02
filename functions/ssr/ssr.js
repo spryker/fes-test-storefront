c67d192dd613dfab11f8b69bee269c473a88809c
Aleksey Belan <alexey.belan@spryker.com>
2022-02-02 18:39:00 +0200

    feat: update ssr.js

--- functions/ssr/ssr.js
+++ functions/ssr/ssr.js
@@ -4,33 +4,33 @@ const config = require('../../nuxt.config');
 const { builder } = require('@netlify/functions');
 
 function createNuxtHandler(nuxtConfig) {
-    const nuxt = new Nuxt({
-        ...config,
-        dev: false,
-        _start: true,
-    });
+  const nuxt = new Nuxt({
+    ...nuxtConfig,
+    dev: false,
+    _start: true,
+  });
 
-    let server = null;
-    return async (event, ctx) => {
-        if (!server) {
-            await nuxt?.ready();
-            server = serverless(nuxt.server.app);
-        }
+  let server = null;
+  return async (event, ctx) => {
+    if (!server) {
+      await nuxt?.ready();
+      server = serverless(nuxt.server.app);
+    }
 
-        const result = await server(event, ctx);
-        // const cacheValue = event.queryStringParameters.ebPreview
-        //   ? 'no-cache'
-        //   : 'public, max-age=31536000';
-        //
-        // return {
-        //   ...result,
-        //   headers: {
-        //     ...result.headers,
-        //     'Cache-Control': cacheValue,
-        //   },
-        // };
-        return result;
-    };
+    const result = await server(event, ctx);
+    // const cacheValue = event.queryStringParameters.ebPreview
+    //   ? 'no-cache'
+    //   : 'public, max-age=31536000';
+    //
+    // return {
+    //   ...result,
+    //   headers: {
+    //     ...result.headers,
+    //     'Cache-Control': cacheValue,
+    //   },
+    // };
+    return result;
+  };
 }
 
 exports.handler = builder(createNuxtHandler(config));
