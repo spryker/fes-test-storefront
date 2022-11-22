3834e8d23bc5563693a7486d961bc8433d7a6fce
Aleksey Belan <alexey.belan@spryker.com>
2022-11-22 16:59:05 +0200

    add lock

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
     root: './src',
     envDir: '../',
     envPrefix: ['FES', 'SCOS', 'STORE'],
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+      sourcemap: true,
+    },
+    server: {
+      port: 3000,
+    },
   };
 });
