da78861140eefc7483ddbb8c20d779a427d64bfd
Aleksey Belan <alexey.belan@spryker.com>
2022-11-23 16:04:36 +0200

    custom theme

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
-import { appBuilder } from '@spryker-oryx/core';
+import { appBuilder, Theme } from '@spryker-oryx/core';
 import { b2cFeatures } from '@spryker-oryx/presets';
 import { storefrontTheme } from '@spryker-oryx/themes';
 
+const customTheme: Theme = {
+  name: 'custom',
+  designTokens: [
+    {
+      color: {
+        brand: 'red',
+      },
+    },
+  ],
+};
+
 export const app = appBuilder()
   .withFeature(b2cFeatures)
-  .withTheme(storefrontTheme)
+  .withTheme([storefrontTheme, customTheme])
   .create();
