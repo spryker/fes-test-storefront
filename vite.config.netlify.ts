7d9d51b29dcc77cc8cfdfaaffa36e47eeb4e7026
Aleksey Belan <alexey.belan@spryker.com>
2022-12-01 02:14:01 +0200

    netlify

---
+++ vite.config.netlify.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+export default defineConfig({
+  root: './src',
+  build: {
+    lib: {
+      entry: '../server/hosting/netlify.ts',
+      formats: ['es'],
+      fileName: 'index',
+    },
+    emptyOutDir: true,
+    outDir: '../dist/functions/ssr',
+    rollupOptions: {
+      external: [
+        'fs',
+        'path',
+        'url',
+        'module',
+        'vm',
+        /^node/,
+        'express',
+        /^@netlify/,
+        'util',
+      ],
+    },
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: '../server/hosting/netlify.toml',
+          dest: '../../client',
+        },
+      ],
+    }),
+  ],
+});
