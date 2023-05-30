fc907bb622457f08c797e699c4e56acd2ac8b742
dunqan <dunqan@w9.pl>
2023-05-30 08:44:00 +0200

    routing fix

--- src/routes/Pages/Pages.tsx
+++ src/routes/Pages/Pages.tsx
@@ -1,11 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 
 import Box from '@mui/material/Box';
 
 import routes from '..';
-import { getPageHeight } from './utils';
+import {getPageHeight} from './utils';
+import {oryxSubApp} from "@/routes/oryx-utils";
 
 function Pages() {
+
+  oryxSubApp('/page-3/');
+
   return (
     <Box sx={{ height: (theme) => getPageHeight(theme) }}>
       <Routes>
